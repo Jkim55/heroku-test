a6ca97cc39bcd7418cc218aca9f374b02b9801a7
Ji Kim <jkim55@gmail.com>
2016-08-25 13:31:52 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const port = process.env.PORT
+let app = express()
+
+app.get('/', (req, res)=>{
+  res.json({message: 'Herrrro! I am working. 😊 '})
+})
+
+app.listen(Number(port), ()=>{
+  console.log('Server running on port: ', port)
+})
